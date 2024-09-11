365222c32e3414aee4d19c8800bcfcb5204a4602
chenxiaolani <2524729719@qq.com>
2024-09-11 10:35:26 +0800

    实现登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -40,5 +43,41 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送验证码：{}", code);
         // 6. 返回成功
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. 校验手机号是否合法
+        boolean phoneInvalid = RegexUtils.isPhoneInvalid(loginForm.getPhone());
+        // 2. 如果不合法， 返回错误信息
+        if (phoneInvalid) {
+            return Result.fail("手机号格式不正确");
+        }
+        // 2. 校验验证码是否正确
+        Object cacheCode = session.getAttribute("code");
+        // 3.不一致，报错
+        if (cacheCode == null || !cacheCode.equals(loginForm.getCode())) {
+            return Result.fail("验证码错误");
+        }
+        // 4. 一致，根据手机号查询用户
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        // 5. 如果用户不存在，直接注册并保存到session
+        if (user == null) {
+            user = createUserWithPhone(loginForm.getPhone());
+        }
+        // 6. 如果用户存在，直接保存到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1. 生成用户信息
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(6));
+        // 2. 保存到数据库
+        save(user);
+        // 3. 返回用户信息
+        return user;
     }
 }
