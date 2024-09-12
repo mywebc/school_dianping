3d233544672f37ab20b9065bb028bffd849bb5c4
chenxiaolani <2524729719@qq.com>
2024-09-12 16:24:00 +0800

    更改条件stock改为>0提高成功率

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,7 +1,9 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -13,4 +15,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
+    @Transactional
+        // 涉及两张表， 加上事务
+    Result seckillVoucher(Long voucherId);
 }
