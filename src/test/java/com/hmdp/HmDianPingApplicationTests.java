fbf763cbc146be181fb115d6520776ac79496eb2
chenxiaolani <2524729719@qq.com>
2024-09-19 11:50:39 +0800

    编写测试用例将店铺数据根据typeID写入redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,11 +1,21 @@
 package com.hmdp;
 
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -16,9 +26,41 @@ class HmDianPingApplicationTests {
     @Resource
     private CacheClient cacheClient;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     void testSaveShop() {
         shopService.cacheShopToRedis(1L, 10L);
+    }
+
+    @Test
+    void loadShopData() {
+        // 先查询所有店铺
+        List<Shop> list = shopService.list();
+        // 根据typeID分组，一个id存一个集合, 这里比较优雅的写法就是Collectors.groupingBy，这样就自动根据typeId分组了
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 分批写入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 获取类型id
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;
+            // 获取该类型的下的所有店铺
+            List<Shop> shops = entry.getValue();
+            List<org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(shops.size());
+
+            // 将店铺写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : shops) {
+                // 存入redis，但是这个方法不优雅，每次循环需要写一次redis
+//                stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            // 优雅的写法，一次性写入，我们把shop全部转为locations，然后一次性写入
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
     }
 
 }
